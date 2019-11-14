04e322b72d6ffe6cbe0aeb38dc653981ce331d28
Matthew <m.massom96@gmail.com>
2019-11-14 15:15:49 -0600

    Simple Classifier

---
+++ clothing_classification.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+# print(train_labels[0])
+# print(train_images[0])
+# plt.imshow(train_images[0], cmap=plt.cm.binary)
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+    ])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+# print("Tested Loss", test_loss)
+# print("Tested Accuracy", test_acc)
+
+prediction = model.predict(test_images)
+# print(np.argmax(prediction[0]))
+
+for i in range(10):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
