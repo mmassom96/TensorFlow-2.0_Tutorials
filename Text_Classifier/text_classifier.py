91e9394dd677d18521e08e7e6a77019c3f42e104
Matthew Masom <m.massom96@gmail.com>
2019-11-14 22:36:36 -0600

    Text Classifier

--- Text_Classifier/text_classifier.py
+++ Text_Classifier/text_classifier.py
@@ -4,9 +4,12 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 # print(train_data[0])
+# print(train_labels[0])
+# print(train_labels[1])
+# print(train_labels[2])
 
 word_index = data.get_word_index()
 
@@ -18,7 +21,40 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen = 256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen = 256)
+
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-print(decode_review(test_data[0]))
+# print(decode_review(test_data[0])+"\n \n" + decode_review(test_data[1])+"\n \n"+decode_review(test_data[2]))
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(88000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+model.summary()
+
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+
+test_review = test_data[0]
+predict = model.predict([test_review])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: " + str(predict[0]))
+print("Actual: " + str(test_labels[0]))
+print(results)
+
+model.save("model.h5")
